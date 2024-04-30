77c38cd269ffafa4ce97787f833fd4b436c23e2c
NSG650 <nsg650@waifu.club>
2024-04-30 17:26:55 +0530

    Got wozmon running. Some what.

--- nsg6502.h
+++ nsg6502.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 - (__DATE__ + 7) NSG650
+ * Copyright 2024 - &__DATE__[7] NSG650
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
